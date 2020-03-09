6b9412dce09fb331895db385562bb7d53b3c08d1
HARIRAK SRITHAM <harirak@hotmail.co.th>
2020-03-10 01:36:02 +0700

    Add project files.

---
+++ WindowsFormsApp2/Archer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Archer
+    {
+        public int ArrowCount { get; set; }
+
+        public Archer(int arrowCount)
+        {
+            this.ArrowCount = arrowCount;
+        }
+
+        public int Shoot()
+        {
+            if (ArrowCount <= 0)
+                return 0;
+            return --ArrowCount;
+        }
+
+        public int Reload()
+        {
+            
+            return ArrowCount += 10;
+        }
+       
+    }
+}
